84a69535c1b5dd5a91c0c0dccf5409df952b941f
Periklis Paganos <51204386+Perpag@users.noreply.github.com>
2025-11-14 16:39:37 +0200

    Update Clustering_analysis_pm_ovary.R

--- Clustering_analysis_pm_ovary.R
+++ Clustering_analysis_pm_ovary.R
@@ -10,21 +10,21 @@ library(EnhancedVolcano)
 set.seed(312)
 
 #loading the data
-#1
+#library_1
 scf1.data <- Read10X(data.dir = "./scf1")
 scf1_barcodes <- read.delim(file = "./scf1/barcodes.tsv", stringsAsFactors = F, header = F)
 scf1 <- CreateSeuratObject(scf1.data, project = "scf1", min.cells = 3, min.features = , meta.data = rownames(scf1_barcodes$v1)) #check values for cells and features
 VlnPlot(scf1, features = c("nFeature_RNA", "nCount_RNA"), ncol = 2)
 scf1<- subset(scf1, subset = nFeature_RNA > 400 & nFeature_RNA < 5000)
 
-#2
+#library_2
 scf5.data <- Read10X(data.dir = "./scf5")
 scf5_barcodes <- read.delim(file = "./scf1/barcodes.tsv", stringsAsFactors = F, header = F)
 scf5 <- CreateSeuratObject(scf5.data, project = "scf5", min.cells = 3, min.features = , meta.data = rownames(scf5_barcodes$v1)) #check values for cells and features
 VlnPlot(scf5, features = c("nFeature_RNA", "nCount_RNA"), ncol = 2)
 scf5<- subset(scf5, subset = nFeature_RNA > 400 & nFeature_RNA < 5000)
 
-#3
+#library_3
 scf9.data <- Read10X(data.dir = "./scf9")
 scf9_barcodes <- read.delim(file = "./scf9/barcodes.tsv", stringsAsFactors = F, header = F)
 scf9 <- CreateSeuratObject(scf9.data, project = "scf9", min.cells = 3, min.features = , meta.data = rownames(scf9_barcodes$v1)) #check values for cells and features
@@ -104,7 +104,7 @@ markers <- FindAllMarkers(object = pm_ovary_integrated_umap, assay = "RNA", only
 epithelium<- WhichCells(pm_ovary_integrated_umap, idents = c("1", "2", "3", "4"))
 follicle1<- WhichCells(pm_ovary_integrated_umap, idents = c("5", "6", "7", "8"))
 follicle2<- WhichCells(pm_ovary_integrated_umap, idents = c("9", "10", "11", "12"))
-germ<- WhichCells(pm_ovary_integrated_umap, idents = c("13", "14"))
+germ_cell_nests<- WhichCells(pm_ovary_integrated_umap, idents = c("13", "14"))
 oocytes<- WhichCells(pm_ovary_integrated_umap, idents = c("15", "16", "17", "18", "19"))
 neurons<- WhichCells(pm_ovary_integrated_umap, idents = "20")
 muscles<- WhichCells(pm_ovary_integrated_umap, idents = c("21", "22"))
@@ -114,7 +114,7 @@ immune<- WhichCells(pm_ovary_integrated_umap, idents = c("26", "27"))
 DimPlot(pm_ovary_integrated_umap, label=TRUE,reduction = "umap", cells.highlight= list(epithelium, neurons,germ,oocytes,muscles, follicle3, follicle1, follicle2, immune), cols.highlight = c("orange", "salmon","pink","#be80ff6d","#00bf7661","green", "yellow", "#3da1ff78"), cols= "lightblue", sizes.highlight = 0.1)     
 
 
-# trajectory analysis with monocle3
+#monocle3
 cds<-subset(pm_ovary_integrated_umap, idents=c("13", "14","15", "16", "17", "18", "19"))
 cds <- as.cell_data_set(cds)
 cds <- cluster_cells(cds, resolution=1e-3)
@@ -127,5 +127,3 @@ plot_cells(cds,
 color_cells_by = "pseudotime",label_cell_groups=FALSE,label_leaves=FALSE,label_branch_points=FALSE,graph_label_size=1.5)
 
 EnhancedVolcano(Supplementary_file_4, lab = Supplementary_file_4$Description, x = "log2FC", y = "pvalue", xlab = bquote(~log[2]~"average fold chage"), ylab = bquote(~Log[10]~italic(pval-adj)), pCutoff = 5e-2, FCcutoff = 1, legendLabels = c("NS", expression(Log[2]~FC, "pval-adj", expression(pval-adj~and~log[2]~FC))), legendPosition = "bottom")
-
-
